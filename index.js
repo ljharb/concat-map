4b53eee40a441cc752e2db093be6c44a92560189
James Halliday <mail@substack.net>
2012-06-07 20:07:03 -0700

    i param test passes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (xs, fn) {
     var res = [];
     for (var i = 0; i < xs.length; i++) {
-        var x = fn(xs[i]);
+        var x = fn(xs[i], i);
         if (Array.isArray(x)) res.push.apply(res, x);
         else res.push(x);
     }
