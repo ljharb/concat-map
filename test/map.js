123d4d64f737831f46adb1b12c3c33e291774307
James Halliday <mail@substack.net>
2012-06-07 20:06:32 -0700

    failing test for i param

--- test/map.js
+++ test/map.js
@@ -3,10 +3,13 @@ var test = require('tap').test;
 
 test('empty or not', function (t) {
     var xs = [ 1, 2, 3, 4, 5, 6 ];
-    var ys = concatMap(xs, function (x) {
+    var ixes = [];
+    var ys = concatMap(xs, function (x, ix) {
+        ixes.push(ix);
         return x % 2 ? [ x - 0.1, x, x + 0.1 ] : [];
     });
     t.same(ys, [ 0.9, 1, 1.1, 2.9, 3, 3.1, 4.9, 5, 5.1 ]);
+    t.same(ixes, [ 0, 1, 2, 3, 4, 5 ]);
     t.end();
 });
 
